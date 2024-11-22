ca38873f27dae44b61ec89dbf1a8d09b67e77375
Глинник Егор Максимович <emglinnik@edu.hse.ru>
2024-11-23 02:02:13 +0300

    Task 2. DONE

--- task2/main.py
+++ task2/main.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import time
+import csv
 
-def count_animals_by_letter(url):
+def count_animals_by_letter(url, output_file):
     base_url = "https://ru.wikipedia.org"
     counts = defaultdict(int)
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     }
-    
+
     while url:
         print(f"Парсим страницу: {url}")
         try:
@@ -24,9 +25,11 @@ def count_animals_by_letter(url):
         items = soup.select(".mw-category-group ul li a")
         for item in items:
             animal_name = item.text.strip()
-            if animal_name:
+            if animal_name and ("А" <= animal_name[0].upper() <= "Я" or animal_name[0].upper() == "Ё"):
                 first_letter = animal_name[0].upper()
                 counts[first_letter] += 1
+            else:
+                url = None
 
         next_page = soup.select_one("a:contains('Следующая страница')")
         if next_page:
@@ -36,10 +39,17 @@ def count_animals_by_letter(url):
 
         time.sleep(0.5)  # Пауза между запросами
 
-    return counts
+    # Сохраняем результат
+    with open(output_file, mode="w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        for letter, count in sorted(counts.items()):
+            writer.writerow([letter, count])
 
-start_url = "https://ru.wikipedia.org/w/index.php?title=Категория:Животные_по_алфавиту"
-result = count_animals_by_letter(start_url)
+    print(f"Результаты сохранены в файл: {output_file}")
 
-for letter, count in sorted(result.items()):
-    print(f"{letter}: {count}")
+
+if __name__ == "__main__":
+    start_url = "https://ru.wikipedia.org/w/index.php?title=Категория:Животные_по_алфавиту"
+    output_file = "task2/beasts.csv"
+
+    count_animals_by_letter(start_url, output_file)
